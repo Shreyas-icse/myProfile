88168d0fe392a5e9b51c5c524f3296bb680717cd
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-05-02 21:46:23 +0530

    updated

--- js/data/projects.js
+++ js/data/projects.js
@@ -5,15 +5,15 @@ const projectsData = [
     description: "A MERN-based blue collar employment platform for job seekers and employers with user profiles, job-related workflows, and platform management.",
     technologies: ["React", "Node.js", "Express.js", "MongoDB"],
     demoLink: "#",
-    githubLink: "#"
+    githubLink: "https://github.com/Shreyas-icse/user-portal"
   },
   {
     title: "LinkedIn Clone",
     category: "backend",
-    description: "A CLI version of a LinkedIn clone built using Node.js, Express.js, and MongoDB to practice backend flow, routing, and data handling.",
-    technologies: ["Node.js", "Express.js", "MongoDB"],
+    description: "A CLI version of a LinkedIn clone built using Node.js, Express.js, to practice backend flow, routing, and data handling.",
+    technologies: ["Node.js", "Express.js"],
     demoLink: "#",
-    githubLink: "#"
+    githubLink: "https://github.com/Shreyas-icse/MERN_Stu_FebMay26Mys/tree/main/Team%20project/LinkedIn%20Clone/MERN_Week1_V1.0"
   },
   {
     title: "BookMyShow Clone",
@@ -21,6 +21,6 @@ const projectsData = [
     description: "A CLI version of a BookMyShow clone built using Node.js, Express.js, and MongoDB for booking logic and database operations.",
     technologies: ["Node.js", "Express.js", "MongoDB"],
     demoLink: "#",
-    githubLink: "#"
+    githubLink: "https://github.com/Shreyas-icse/MERN_Stu_FebMay26Mys/tree/main/Projects/Guided%20Project/BookMyShowClone/MERN_Week2_V1"
   }
 ];
